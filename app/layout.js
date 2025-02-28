e30e9b4dd35c4ec48bdcd9cb6fee246a9d7583f6
Sadegh Khavari <sadeghkhawari@outlook.com>
2025-02-28 20:33:32 +0330

    Build the about page

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto bg-red-800">{children}</main>
+          <main className="max-w-7xl mx-auto ">{children}</main>
         </div>
       </body>
     </html>
