cd4bbce759282b87069cf854996843cab8369916
Sadegh Khavari <sadeghkhawari@outlook.com>
2025-02-28 19:26:43 +0330

    Build home page
    Optimize images using the Image tag and building the home page.

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -21,14 +20,12 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-
-        <main>{children}</main>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto bg-red-800">{children}</main>
+        </div>
       </body>
     </html>
   );
