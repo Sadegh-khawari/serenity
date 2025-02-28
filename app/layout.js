f202fb14bc816de591058de565c67ba7a58f474f
Sadegh Khavari <sadeghkhawari@outlook.com>
2025-02-28 18:20:48 +0330

    Add the style to the project
    Configure the tailwind css and adding the metadata and favicon and in
    the end loading and optimization for fonts.

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,28 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: "The Serenity Stays",
+  title: {
+    template: "%s / The Serenity Stays",
+    default: "Welcome / The Serenity Stays",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
