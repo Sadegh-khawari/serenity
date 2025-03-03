c181073aff24a30b8ade430eb6096c42a24fe535
Sadegh Khavari <sadeghkhawari@outlook.com>
2025-03-03 20:08:26 +0330

    Fetch and display cabin list

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qwvzcbjcjfwiysnpyple.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
   webpack: (config) => {
     config.watchOptions = {
       poll: 1000, // Check for changes every second
