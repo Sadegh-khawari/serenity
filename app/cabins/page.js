a1936c36fb51a16750fbd758909d3bf699a4650e
Sadegh Khavari <sadeghkhawari@outlook.com>
2025-04-02 15:59:45 +0330

    Advance server components in client components

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -3,7 +3,6 @@ import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
 import Filter from "@/app/_components/Filter";
 
-export const revalidate = 3600;
 
 export const metadata = {
   title: "Cabins",
