b161656ad3ef78d3b74cbad052bdc0e949c90932
Sadegh Khavari <sadeghkhawari@outlook.com>
2025-05-19 20:24:52 +0330

    make the cabin card mobile responsive

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -1,55 +1,53 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { UsersIcon } from '@heroicons/react/24/solid';
+import Image from "next/image";
+import Link from "next/link";
+import { UsersIcon } from "@heroicons/react/24/solid";
 
 function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
 
   return (
-    <div className='flex border-primary-800 border'>
-      <div className='flex-1 relative'>
+    <div className="flex flex-col sm:flex-row border-primary-800 border">
+      <div className="relative h-[200px] sm:h-auto sm:flex-1">
         <Image
           src={image}
           fill
           alt={`Cabin ${name}`}
-          className='object-cover border-r border-primary-800'
+          className="object-cover border-b sm:border-b-0 sm:border-r border-primary-800"
         />
       </div>
 
-      <div className='flex-grow'>
-        <div className='pt-5 pb-4 px-7 bg-primary-950'>
-          <h3 className='text-accent-500 font-semibold text-2xl mb-3'>
+      <div className="flex-grow">
+        <div className="p-5 bg-primary-950">
+          <h3 className="text-accent-500 font-semibold text-2xl mb-3">
             Cabin {name}
           </h3>
 
-          <div className='flex gap-3 items-center mb-2'>
-            <UsersIcon className='h-5 w-5 text-primary-600' />
-            <p className='text-lg text-primary-200'>
-              For up to <span className='font-bold'>{maxCapacity}</span> guests
-            </p>
+          <div className="flex items-center gap-2 mb-4">
+            <UsersIcon className="h-5 w-5 text-primary-600" />
+            <span className="text-primary-200">Up to {maxCapacity} guests</span>
           </div>
 
-          <p className='flex gap-3 justify-end items-baseline'>
+          <div className="flex items-baseline gap-2">
             {discount > 0 ? (
               <>
-                <span className='text-3xl font-[350]'>
+                <span className="text-3xl font-[350]">
                   ${regularPrice - discount}
                 </span>
-                <span className='line-through font-semibold text-primary-600'>
+                <span className="line-through text-primary-600">
                   ${regularPrice}
                 </span>
               </>
             ) : (
-              <span className='text-3xl font-[350]'>${regularPrice}</span>
+              <span className="text-3xl font-[350]">${regularPrice}</span>
             )}
-            <span className='text-primary-200'>/ night</span>
-          </p>
+            <span className="text-primary-200">/ night</span>
+          </div>
         </div>
 
-        <div className='bg-primary-950 border-t border-t-primary-800 text-right'>
+        <div className="bg-primary-950 border-t border-b border-primary-800">
           <Link
             href={`/cabins/${id}`}
-            className='border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900'
+            className="block py-4 px-6 text-center hover:bg-accent-600 transition-all hover:text-primary-900"
           >
             Details & reservation &rarr;
           </Link>
